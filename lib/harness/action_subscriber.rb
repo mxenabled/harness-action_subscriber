350a4832b62b1b65bfe7af12db027d45ca183734
hqmq <michael@riesd.com>
2014-07-08 16:41:51 -0600

    more specific statsd names

--- lib/harness/action_subscriber.rb
+++ lib/harness/action_subscriber.rb
@@ -2,17 +2,20 @@ require "harness/action_subscriber/version"
 
 ::ActiveSupport::Notifications.subscribe "popped_event.action_subscriber" do |*args|
   event = ::ActiveSupport::Notifications::Event.new(*args)
-  ::Harness.increment "action_subscriber.popped_event.#{event.payload[:queue]}"
-  ::Harness.gauge "action_subscriber.popped_event_bytes.#{event.payload[:queue]}", event.payload[:payload_size]
+  event_name = event.payload[:queue].gsub '.', '-'
+  ::Harness.increment "action_subscriber.popped_event.#{event_name}"
+  ::Harness.gauge "action_subscriber.popped_event_bytes.#{event_name}", event.payload[:payload_size]
 end
 
 ::ActiveSupport::Notifications.subscribe "received_event.action_subscriber" do |*args|
   event = ::ActiveSupport::Notifications::Event.new(*args)
-  ::Harness.increment "action_subscriber.received_event.#{event.payload[:queue]}"
-  ::Harness.gauge "action_subscriber.received_event_bytes.#{event.payload[:queue]}", event.payload[:payload_size]
+  event_name = event.payload[:queue].gsub '.', '-'
+  ::Harness.increment "action_subscriber.received_event.#{event_name}"
+  ::Harness.gauge "action_subscriber.received_event_bytes.#{event_name}", event.payload[:payload_size]
 end
 
 ::ActiveSupport::Notifications.subscribe "process_event.action_subscriber" do |*args|
   event = ::ActiveSupport::Notifications::Event.new(*args)
-  ::Harness.timing "action_subscriber.process_event.#{event.payload[:subscriber]}", event.duration
+  event_name = event.payload[:routing_key].gsub '.', '-'
+  ::Harness.timing "action_subscriber.process_event.#{event_name}", event.duration
 end
